0abf788546ac133fa29822727784a27efd454f55
Mohammad Zaid <mdzaid786291@gmail.com>
2024-03-26 22:08:36 +0530

    dkjfkl

--- try.py
+++ try.py
@@ -5,4 +5,9 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret ,frame = cap.read()
+    cv2.imshow("ddkl", frame)
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
 
+cv2.
+cv2.destroyAllWindows()
