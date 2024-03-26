00a77237cc55ef83132c20a97fd0d26091e20052
Mohammad Zaid <mdzaid786291@gmail.com>
2024-03-26 22:09:45 +0530

    kdljf

--- try.py
+++ try.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while True:
     ret ,frame = cap.read()
     cv2.imshow("ddkl", frame)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cv2.
+cap.release()
 cv2.destroyAllWindows()
